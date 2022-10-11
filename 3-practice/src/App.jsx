f2960d8a85f0df8ca56fa9fb9698e290523e0341
Baiysh <115449261+kydyrmyshovbaiysh@users.noreply.github.com>
2022-10-11 12:09:59 +0600

    add autorization functionality

---
+++ 3-practice/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+	return (
+		<div className='App'>
+			<p>
+				Проект: <b>TO-DO APP</b>
+			</p>
+			<p>
+				Разработчик:№1 <b>Байыш</b>
+				<span>Написал код авторизации</span>
+			</p>
+		</div>
+	)
+}
+
+export default App
